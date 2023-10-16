05a39249b478ad5569a7545a12cdc4bd4e85bdb0
patrick <patrickvafaie@cmail.carleton.ca>
2023-10-15 20:26:31 -0400

    Fix HTTP tests

--- src/test/java/com/example/addressbook/HttpRequestTest.java
+++ src/test/java/com/example/addressbook/HttpRequestTest.java
@@ -1,5 +1,7 @@
 package com.example.addressbook;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,15 +33,55 @@ class HttpRequestTest {
 
 		ResponseEntity<String> responseEntity = this.restTemplate.exchange(
 				"http://localhost:" + port + "/addressBooks",
-				HttpMethod.PUT,
+				HttpMethod.POST,
 				requestEntity,
 				String.class
 		);
 
 		String response = responseEntity.getBody();
 
-		System.out.println(response);
+		assertThat(responseEntity.getStatusCodeValue()).isEqualTo(201); // Check for HTTP 201 Created
 
-		assertThat(response).contains("Your expected content here");
+		// Parse the response JSON to verify its structure
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode jsonNode = objectMapper.readTree(response);
+
+		// Check if "numBuddies" is present and has a value of 0
+		assertThat(jsonNode.path("numBuddies").asInt()).isEqualTo(0);
+
+		// Check for the presence of "_links" in the response
+		assertThat(jsonNode.has("_links")).isTrue();
+	}
+
+	@Test
+	public void testAddBuddyInfo() throws Exception {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Content-Type", "application/json");
+
+		// JSON request body for adding a BuddyInfo
+		String jsonRequest = "{ \"name\": \"Patrick\", \"phoneNumber\": \"123456789\" }";
+
+		HttpEntity<String> requestEntity = new HttpEntity<>(jsonRequest, headers);
+
+		ResponseEntity<String> responseEntity = this.restTemplate.exchange(
+				"http://localhost:" + port + "/buddyInfoes",
+				HttpMethod.POST, // Use POST to create a new BuddyInfo
+				requestEntity,
+				String.class
+		);
+
+		String response = responseEntity.getBody();
+
+		assertThat(responseEntity.getStatusCodeValue()).isEqualTo(201); // Check for HTTP 201 Created
+
+		// Parse the response JSON to verify its structure
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode jsonNode = objectMapper.readTree(response);
+
+		// Optionally, check the response for specific information
+		assertThat(jsonNode.has("name")).isTrue();
+		assertThat(jsonNode.path("name").asText()).isEqualTo("Patrick");
+		assertThat(jsonNode.has("phoneNumber")).isTrue();
+		assertThat(jsonNode.path("phoneNumber").asText()).isEqualTo("123456789");
 	}
 }
